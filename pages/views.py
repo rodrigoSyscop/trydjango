e6cf07803f17a8bbc5bc49e08e39bf20ee003d9f
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-08-18 02:16:33 -0300

    Designate root urls to pages' urls module

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home_view(*args, **kwargs):
+def index(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
+
+def about(*args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
+
+def contact(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from pages.views import home_view
 
 urlpatterns = [
-    path('', home_view ),
+    path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
