3a4681eb30596fa2b4a04a07ced6370f5122b536
Rodrigo Vieira <rodrigodelimavieira@gmail.com>
2018-08-18 15:05:44 -0300

    Added product model form

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     # ex: /products/5/
-    path('<int:product_id>/', views.product_detail_view, name='products_detail')
+    path('<int:product_id>/', views.product_detail_view, name="product_detail"),
+    path('create/', views.product_create_view, name="product_create")
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from .forms.product import ProductForm
 from .models import Product
 
 def product_detail_view(request, product_id):
@@ -8,3 +9,15 @@ def product_detail_view(request, product_id):
         "product": product
     }
     return render(request, 'products/detail.html', context)
+
+
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "products/product_create.html", context)
